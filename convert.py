a3b9f57502d8a15f9166efbdfd71974625100e94
Stefan Schulte <st.schulte@fz-juelich.de>
2022-03-11 15:09:35 +0100

    conversion

--- convert.py
+++ convert.py
@@ -13,14 +13,18 @@ dataline = 0
 count = 0
 dacdepth = 0
 preampgain = 0
+zpiezoconst = 0
 
 for line in lines:
     count += 1
     if "DAC-Type" in line:
-        dacdepth = line.split("=")[1].replace("bit","")
+        dacdepth = int(line.split("=")[1].replace("bit",""))
     if "Gainpreamp" in line:
-        preampgain = line.split("=")[1]
-        print(preampgain)
+        preampgain = int(line.split("=")[1])
+    if "ZPiezoconst" in line:
+        zpiezoconst = float(line.split("=")[1])
+        print(zpiezoconst)
+
     if line == "DATA":
         dataline = count
 
@@ -37,7 +41,7 @@ param = re.findall('([^-_\s]+)', lines[dataline])
 
 # In Version 3, the Channel-List has to be decoded:
 channellist = {
-    "Current" : 1, 
+    "current" : 1, 
     "dI/dV"   : 2, 
     "d2I/dV2" : 4, 
     "ADC0"    : 8, 
@@ -53,15 +57,26 @@ channellist = {
 }
 
 if version == "[ParVERT32]":
-    columnnames = ["v4", "bias", "z"]
+    columnnames = ["bias", "z", "unknown"]
 else:
     columnnames = ["bias", "z"]
 
 for channel in channellist:
     if int(param[3]) & channellist[channel] > 0:
         columnnames.append(channel)
+columnnames.append("NaN")
+ADCtoV = 20.0 / 2 ** dacdepth
+ADCtoI = 20.0 / 2 ** dacdepth / 10 ** (preampgain - 12) * 10 ** (-12)
 
-print(columnnames)
-data = pd.read_csv('test.VERT', delimiter='\t', skiprows=dataline+1, encoding='unicode_escape', encoding_errors='ignore', header=None,index_col=False)
-fig = px.line(data)
+data = pd.read_csv('test.VERT', delimiter='\t', skiprows=dataline+1, encoding='unicode_escape', encoding_errors='ignore',index_col=0,names=columnnames)
+data = data.iloc[: , :-1]
+data["current"] *= ADCtoI
+data["bias"] *= 1e-3
+data["ADC1"] *= ADCtoV
+data["ADC2"] *= ADCtoV
+data["z"] *= zpiezoconst / 1000 * 0.0000000001
+
+
+print(data)
+fig = px.line(data, x="bias", y=["current", "ADC1", "ADC2"])
 fig.show()
