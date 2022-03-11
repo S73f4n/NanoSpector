07c93ab4785050b6b6589ffe71c2857c05e8d0d1
Stefan Schulte <st.schulte@fz-juelich.de>
2022-03-11 13:29:44 +0100

    read params wip

--- convert.py
+++ convert.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
+import re
+
 pio.renderers.default = "vscode"
 
 
@@ -9,11 +11,20 @@ with open('test.VERT', 'r', errors="ignore") as f:
 
 dataline = 0
 count = 0
+dacdepth = 0
+preampgain = 0
 
 for line in lines:
     count += 1
+    if "DAC-Type" in line:
+        dacdepth = line.split("=")[1].replace("bit","")
+    if "Gainpreamp" in line:
+        preampgain = line.split("=")[1]
+        print(preampgain)
     if line == "DATA\n":
         dataline = count
+
+param = re.findall('([^-_\s]+)', lines[dataline])
 
 data = pd.read_csv('test.VERT', delimiter='\t', skiprows=dataline+1, encoding='unicode_escape', encoding_errors='ignore', header=None)
 fig = px.line(data)
