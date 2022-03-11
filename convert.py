d5644f9ff3a9d21394503a2f92ec1dfe0a316970
Stefan Schulte <st.schulte@fz-juelich.de>
2022-03-11 10:30:28 +0100

    read pandas

---
+++ convert.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+with open('test.VERT', 'r', errors="ignore") as f:
+    lines = f.readlines()
+
+dataline = 0
+count = 0
+
+for line in lines:
+    count += 1
+    if line == "DATA\n":
+        dataline = count
+
+data = pd.read_csv('test.VERT', delimiter='\t', skiprows=dataline+1, encoding='unicode_escape', error_bad_lines=False)
