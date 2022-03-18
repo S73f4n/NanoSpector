43efe488db1ed55e15ca3d01f8e3d93d528af420
Stefan Schulte <st.schulte@fz-juelich.de>
2022-03-18 14:56:19 +0100

    matplotlib wip

--- gui.py
+++ gui.py
@@ -5,6 +5,11 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
+from matplotlib.backends.backend_gtk3agg import (
+    FigureCanvasGTK3Agg as FigureCanvas)
+from matplotlib.figure import Figure
+import numpy as np
+
 class Handler:
     def on_mainwindow_destroy(self, *args):
         Gtk.main_quit()
@@ -28,6 +33,11 @@ class Handler:
         if treeiter is not None:
             print("You selected", model[treeiter][0])
 
+fig = Figure(figsize=(5, 4), dpi=100)
+ax = fig.add_subplot()
+t = np.arange(0.0, 3.0, 0.01)
+s = np.sin(2*np.pi*t)
+ax.plot(t, s)
 
 builder = Gtk.Builder()
 builder.add_from_file("main.glade")
